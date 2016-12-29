833991674333269bdbaca3faa5bc24f90eea2a3a
Ricky Wang <Ricky Wang>
2016-12-30 00:38:18 +0800

    new note about career

---
+++ moveHtmlToDocs.bat
@@ -0,0 +1,4 @@
+move "*.html" "docs\"
+move "images" "docs\"
+move "js" "docs\"
+move "theme" "docs\"
---
+++ slide_config.js
@@ -0,0 +1,26 @@
+var SLIDE_CONFIG = {
+   // Slide Settings
+   settings: {
+     title: '成為優秀的工程師', 
+     subtitle: '本簡報是關於「成為優秀的工程師」的相關資訊與筆記', 
+     useBuilds: true, // Default: true. False will turn off slide animation builds. 
+     usePrettify: true, // Default: true 
+     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+     favIcon: 'images/fatfingererr.png', 
+     fonts: [
+       'Open Sans:regular,semibold,italic,italicsemibold',
+       'Source Code Pro'
+     ],
+   }, 
+ 
+   // Author information
+   presenters: [{
+     name: 'fatfingererr (fatfingererr@gmail.com)', 
+     company: 'fatfingererr', 
+     gplus: 'https://plus.google.com/fatfingererr', 
+     twitter: 'fatfingererr', 
+     www: 'http://ffe.tw/', 
+     github: 'http://github.com/fatfingererr', 
+   }]
+};
