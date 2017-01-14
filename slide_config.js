462277a15fae3ab8f0c4915ccf8ecc7c9edaeb5d
Ricky Wang <Ricky Wang>
2017-01-14 10:04:08 +0800

    update research note

---
+++ slide_config.js
@@ -0,0 +1,26 @@
+var SLIDE_CONFIG = {
+   // Slide Settings
+   settings: {
+     title: '近期研究概況', 
+     subtitle: '關於近期的各項研究方向的進度與發展 ( 2016 / 1 / 14 )', 
+     useBuilds: true, // Default: true. False will turn off slide animation builds. 
+     usePrettify: true, // Default: true 
+     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+     favIcon: 'images/fatfingererr.png', 
+     fonts: [
+       'Open Sans:regular,semibold,italic,italicsemibold',
+       'Source Code Pro'
+     ],
+   }, 
+ 
+   // Author information
+   presenters: [{
+     name: 'ffe (fatfingererr@gmail.com)', 
+     company: '<a href="http://www.ffe.tw/">http://www.ffe.tw/</a>', 
+     gplus: 'https://plus.google.com/fatfingererr', 
+     twitter: 'fatfingererr', 
+     www: 'http://ffe.tw/', 
+     github: 'http://github.com/fatfingererr', 
+   }]
+};
